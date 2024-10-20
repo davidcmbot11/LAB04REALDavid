b0740748d8da285ea51aba0ae289e4dc09344d29
DavidColque <sis838.davidcolque@gmail.com>
2024-10-20 19:08:56 -0400

    agregated lab04

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { WapperList } from './componets/WrapperList';
+import { List } from './componets/List';
+
+function App() {
+  return (
+    <>
+      <WapperList>
+       <List/> 
+      </WapperList>
+    </>
+  )
+}
+
+export default App
